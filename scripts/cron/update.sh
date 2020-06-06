02845253ef32356c005f43d293ae2e100de82236
Matthew Rayner <m@rayner.io>
2020-06-07 00:01:51 +0100

    Add specific docker compose file calls in update.sh

--- scripts/cron/update.sh
+++ scripts/cron/update.sh
@@ -2,6 +2,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo killall chromium-browse
-docker-compose -f ~/docker-compose.meeting-display.yml stop
-docker-compose -f ~/docker-compose.meeting-display.yml rm -f
+docker-compose -f /home/pi/meeting-display/docker-compose.meeting-display.yml stop
+docker-compose -f /home/pi/meeting-display/docker-compose.meeting-display.yml rm -f
 /home/pi/meeting-display/kiosk.sh
