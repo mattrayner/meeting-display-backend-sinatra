6d6003621288a1480c54ddcaa8f5f549f2cfd563
Matt Rayner <mattrayner@users.noreply.github.com>
2020-06-09 22:28:02 +0100

    add auto-remove command

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,6 +23,7 @@ curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt-get install -y python3 python3-pip
+sudo apt autoremove -y
 sudo apt-get remove python-configparser
 sudo pip3 install docker-compose
 
